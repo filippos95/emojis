c64f53dbdb61741389493b04f5417a3f18ff3897
Filippos Kasioulis <fkasioulis.95@gmail.com>
2025-02-16 18:45:01 +0200

    Update replicate.ts

--- src/server/replicate.ts
+++ src/server/replicate.ts
@@ -16,7 +16,7 @@ export class ReplicateClient {
   async createEmoji({ id, prompt }: { id: string; prompt: string }) {
     const webhook = new URL(`${SITE_URL}/api/webhook/remove-background`)
     webhook.searchParams.set("id", id)
-    webhook.searchParams.set("secret", process.env.API_SECRET as string)
+    webhook.searchParams.set("secret", process.env.WEBHOOK_SECRET as string)
 
     return this.replicate.predictions.create({
       version: "dee76b5afde21b0f01ed7925f0665b7e879c50ee718c5f78a9d38e04d523cc5e",
